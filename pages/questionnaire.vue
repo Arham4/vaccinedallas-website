29c532818b29ef1fda680a5cdee9e08dd89d0b63
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-03-31 15:08:23 -0500

    Add new pictures for questionnaire page

--- pages/questionnaire.vue
+++ pages/questionnaire.vue
@@ -4,14 +4,14 @@
 
     <OffCanvasMobileMenu />
 
-    <Breadcrumb :items="items" title="Before we schedule your vaccine(s)" />
+    <Breadcrumb :items="items" title="Booking Questionnaire" />
 
     <div class="eligibility-area bg_color--1 pt--110 pt_md--80 pt_sm--60 pb--90 pb_md--55 pb_sm--30">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6 col-md-12 col-sm-12 col-12">
             <div class="thumbnail wow move-up animate">
-              <img src="/img/eligibility/injection.jpg" alt="doctor injecting vaccine to a patient">
+              <img src="/img/questionnaire/injection.png" alt="doctor injecting vaccine to a patient">
             </div>
           </div>
           <div class="col-lg-6 col-md-12 col-sm-12 col-12">
@@ -54,7 +54,7 @@ export default {
 
   head () {
     return {
-      title: 'VaccineDallas - Questionnaire'
+      title: 'VaccineDallas - Booking Questionnaire'
     }
   }
 }
